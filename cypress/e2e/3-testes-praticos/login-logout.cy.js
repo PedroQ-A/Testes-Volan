949deb60cdf935d200e8ce78a0415938a9e08025
pedro lucas aguirre <pedrolucasaguirre0103@gmail.com>
2025-10-20 17:59:04 -0400

    Cenarios negativos e hooks

--- cypress/e2e/3-testes-praticos/login-logout.cy.js
+++ cypress/e2e/3-testes-praticos/login-logout.cy.js
@@ -2,9 +2,15 @@
 
 describe('Teste Login', () => {
 
-    it('Deve fazer login', () => {
-
+    beforeEach(() => {
         cy.visit('https://med.volan.app.br/client/login')
+    })
+
+    afterEach(() => {
+        cy.screenshot()
+    })
+
+    it('Deve fazer login', () => {
         cy.get('[data-cy="txtFieldEmail"]').type('pedrolucas0130@admin.volan.app.br')
         cy.get('[data-cy="txtFieldPassword"]').type('Ped0118')
         cy.get('.Login_buttonBox__tK6r > .col > .v-btn').click()
@@ -13,7 +19,20 @@ describe('Teste Login', () => {
         cy.get('.v-list-item__action > .v-btn > .v-btn__content > .v-icon').click()
         cy.wait(1000)
         cy.get('.v-list-item--link > .v-list-item__title').click()
-    
         cy.get('h4').should('contain' , 'Bem vindo ao Volan Doutor')
     });
-});
+
+    it('Deve exibir mensagem de erro ao colocar usuario invalido', () => {
+        cy.get('[data-cy="txtFieldEmail"]').type('pedrolucas@admin.volan.app.br')
+        cy.get('[data-cy="txtFieldPassword"]').type('Ped0118')
+        cy.get('.Login_buttonBox__tK6r > .col > .v-btn').click()
+        cy.get('#swal2-title').should('exist')
+    });
+
+    it('Deve exibir mensagem de erro ao colocar senha invalida', () => {
+        cy.get('[data-cy="txtFieldEmail"]').type('pedrolucas0130@admin.volan.app.br')
+        cy.get('[data-cy="txtFieldPassword"]').type('Pe0118')
+        cy.get('.Login_buttonBox__tK6r > .col > .v-btn').click()
+        cy.get('#swal2-title').should('exist')
+    });
+})
